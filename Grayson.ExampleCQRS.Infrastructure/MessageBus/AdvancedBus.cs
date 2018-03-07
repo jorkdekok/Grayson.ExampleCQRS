5c20ca445039592aeba039de98dedbf320147c13
Jork de Kok GSD <jork.de.kok@grayson.nl>
2018-03-07 21:38:14 +0100

    advanced bus v1

---
+++ Grayson.ExampleCQRS.Infrastructure/MessageBus/AdvancedBus.cs
@@ -0,0 +1,43 @@
+﻿using Grayson.Utils.DDD;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grayson.ExampleCQRS.Infrastructure.MessageBus
+{
+    public class SimpleBus : IBus
+    {
+        private readonly Container _container;
+        private static IList<Type> _registeredHandlers = new List<Type>();
+
+        public SimpleBus()
+        {
+            _container = new Container();
+
+            AutoRegisterCommandHandlers();
+        }
+
+        private void AutoRegisterCommandHandlers()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine(assembly.GetName());
+            }
+            _container.Register(typeof(ICommandHandler<>), assemblies);
+        }
+
+        public void RegisterHandler<TCommandHandler, TInstance>()
+        {
+            _container.Register(typeof(TCommandHandler), typeof(TInstance));
+        }
+
+        public void Send<T>(T command) where T : ICommand
+        {
+            var instance = _container.GetInstance<ICommandHandler<T>>();
+            instance.When(command);
+        }
+    }
+}
--- Grayson.ExampleCQRS.Infrastructure/MessageBus/SimpleBus.cs
+++ Grayson.ExampleCQRS.Infrastructure/MessageBus/SimpleBus.cs
@@ -7,37 +7,27 @@ using System.Text;
 
 namespace Grayson.ExampleCQRS.Infrastructure.MessageBus
 {
-    public class SimpleBus : IBus
+    public class AdvancedBus : IBus
     {
-        private readonly Container _container;
-        private static IList<Type> _registeredHandlers = new List<Type>();
 
-        public SimpleBus()
+        public AdvancedBus()
         {
-            _container = new Container();
-
-            AutoRegisterCommandHandlers();
+            
         }
 
-        private void AutoRegisterCommandHandlers()
+        public void Configure()
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in assemblies)
-            {
-                Console.WriteLine(assembly.GetName());
-            }
-            _container.Register(typeof(ICommandHandler<>), assemblies);
+
         }
 
         public void RegisterHandler<TCommandHandler, TInstance>()
         {
-            _container.Register(typeof(TCommandHandler), typeof(TInstance));
+            
         }
 
         public void Send<T>(T command) where T : ICommand
         {
-            var instance = _container.GetInstance<ICommandHandler<T>>();
-            instance.When(command);
+            
         }
     }
 }

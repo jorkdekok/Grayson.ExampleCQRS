a4bb44cab378794d33fe1e6b81830c8c399175e5
Jork de Kok GSD <jork.de.kok@grayson.nl>
2018-03-08 09:20:34 +0100

    test infra

---
+++ Grayson.ExampleCQRS.Infrastructure.Test/MassTransitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grayson.ExampleCQRS.Infrastructure.Test
+{
+    [TestClass]
+    public class MassTransitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

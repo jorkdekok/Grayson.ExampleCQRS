4eb6d74e10f1f3f06734666be54e06f8375246cc
Jork de Kok GSD <jork.de.kok@grayson.nl>
2018-04-07 11:17:36 +0200

    rename file

--- Grayson.ExampleCQRS.Infrastructure/MessageBus/DefaultRabbitMQPersistentConnection.cs
+++ Grayson.ExampleCQRS.Infrastructure/MessageBus/DefaultRabbitMQPersistentConnection.cs
@@ -8,7 +8,7 @@ using System;
 using System.IO;
 using System.Net.Sockets;
 
-namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBusRabbitMQ
+namespace Grayson.ExampleCQRS.Infrastructure.MessageBus
 {
     public class DefaultRabbitMQPersistentConnection
        : IRabbitMQPersistentConnection

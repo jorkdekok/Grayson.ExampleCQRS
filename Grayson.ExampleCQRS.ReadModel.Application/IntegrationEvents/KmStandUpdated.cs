f7d91cc893308eeb1d5ba08069ca174699850f69
Jork de Kok GSD <jork.de.kok@grayson.nl>
2018-04-11 16:00:40 +0200

    logging and readmodel queue setup

---
+++ Grayson.ExampleCQRS.ReadModel.Application/IntegrationEvents/KmStandUpdated.cs
@@ -0,0 +1,22 @@
+﻿using Grayson.SeedWork.DDD.Application.Integration;
+
+using System;
+
+namespace Grayson.ExampleCQRS.Ritten.Application.IntegrationEvents
+{
+    public class KmStandUpdated : IntegrationEvent
+    {
+        public Guid AdresId { get; private set; }
+        public DateTime Datum { get; private set; }
+        public Guid Id { get; private set; }
+        public int Stand { get; private set; }
+
+        public KmStandUpdated(Guid id, int stand, DateTime datum, Guid adresId)
+        {
+            Id = id;
+            Stand = stand;
+            Datum = datum;
+            AdresId = adresId;
+        }
+    }
+}

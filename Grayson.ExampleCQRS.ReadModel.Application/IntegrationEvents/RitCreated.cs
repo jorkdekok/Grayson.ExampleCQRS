b1d2e4855167e2a30ff7f973d1377f8d96a7e149
Jork de Kok GSD <jork.de.kok@grayson.nl>
2018-05-23 15:33:30 +0200

    RitCreated and ritUpdated events to readmodel

---
+++ Grayson.ExampleCQRS.ReadModel.Application/IntegrationEvents/RitCreated.cs
@@ -0,0 +1,30 @@
+﻿using Grayson.SeedWork.DDD.Application.Integration;
+
+using System;
+
+namespace Grayson.ExampleCQRS.ReadModel.Application.IntegrationEvents
+{
+    public class RitCreated : IntegrationEvent
+    {
+        public readonly string Name;
+
+        public int BeginStand { get; private set; }
+
+        public Guid BeginStandId { get; private set; }
+
+        public int EindStand { get; private set; }
+
+        public Guid EindStandId { get; private set; }
+        public Guid Id { get; private set; }
+
+        public RitCreated(string name, int beginStand, Guid beginStandId, int eindStand, Guid eindStandId, Guid id)
+        {
+            Name = name;
+            BeginStand = beginStand;
+            BeginStandId = beginStandId;
+            EindStand = eindStand;
+            EindStandId = eindStandId;
+            Id = id;
+        }
+    }
+}
---
+++ Grayson.ExampleCQRS.ReadModel.Application/IntegrationEvents/RitUpdated.cs
@@ -0,0 +1,26 @@
+﻿using Grayson.SeedWork.DDD.Application.Integration;
+
+using System;
+
+namespace Grayson.ExampleCQRS.ReadModel.Application.IntegrationEvents
+{
+    public class RitUpdated : IntegrationEvent
+    {
+        public int BeginStand { get; private set; }
+        public Guid BeginStandId { get; private set; }
+        public int EindStand { get; private set; }
+        public Guid EindStandId { get; private set; }
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public RitUpdated(int beginStand, Guid beginStandId, int eindStand, Guid eindStandId, Guid id, string name)
+        {
+            BeginStand = beginStand;
+            BeginStandId = beginStandId;
+            EindStand = eindStand;
+            EindStandId = eindStandId;
+            Id = id;
+            Name = name;
+        }
+    }
+}
